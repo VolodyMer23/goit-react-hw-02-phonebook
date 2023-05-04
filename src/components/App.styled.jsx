22506f4b3189ee99ca0cfa81dc700bf9911c29b3
Volodymyr <mv.design8@gmail.com>
2023-05-04 19:04:45 +0300

    start

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 40;
+  color: #010101;
+  background-color: #cad6e4;
+  padding: 32px;
+`;
