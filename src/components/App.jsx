144a601ca316428fb1de3fe59d05eb1d9788a4f0
Volodymyr <mv.design8@gmail.com>
2023-05-06 03:42:18 +0300

    refact_some_code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,12 @@ import { Phonebook } from './Phonebook/Phonebook';
 import { nanoid } from 'nanoid';
 import Contacts from './Phonebook/ContactsList/ContactList';
 import Filter from './Phonebook/Filter/Filter';
+import ContactsData from './Phonebook/Data/ContactsData.json';
+
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: ContactsData,
     filter: '',
   };
 
